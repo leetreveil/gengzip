513941d892ffe49238fe5f51b9f38ae9c4fd92e2
Lee Treveil <leetreveil@gmail.com>
2012-10-18 22:15:09 +0100

    add setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name = 'gengzip',
+    version = '0.1.0',
+    author = 'Lee Treveil',
+    author_email = 'leetreveil@gmail.com',
+    packages = ['gengzip'],
+    url = 'https://github.com/leetreveil/gengzip',
+    description = 'A python module for gzipping data in python with generators',
+)
