f6d920d7587b3ae53c1c1fed30509a797c95e616
Lee Treveil <leetreveil@gmail.com>
2012-11-06 00:26:21 +0000

    move tests into their own file

---
+++ test.py
@@ -0,0 +1,50 @@
+import unittest
+import StringIO
+import gzip
+from gengzip import compress
+
+def decompress_data(fileobj):
+    gfile = gzip.GzipFile(fileobj=fileobj, mode='rb')
+    result = gfile.read()
+    gfile.close()
+    return result
+
+class tests(unittest.TestCase):
+
+    def test_should_be_able_to_compress_a_list_of_data(self):
+        input = ['123', '45']
+
+        sio = StringIO.StringIO()
+        for compressed in compress(input):
+            sio.write(compressed)
+        sio.seek(0)
+
+        result = decompress_data(sio)
+        self.assertEqual(result, ''.join(input))
+
+    def test_should_be_able_to_compress_data_coming_from_a_generator(self):
+        input = '12345'
+        def gen():
+            yield input
+
+        sio = StringIO.StringIO()
+        for compressed in compress(gen()):
+            sio.write(compressed)
+        sio.seek(0)
+
+        result = decompress_data(sio)
+        self.assertEqual(result, input)
+
+    def test_should_be_able_to_compress_0_bytes_of_data(self):
+        input = ['']
+
+        sio = StringIO.StringIO()
+        for compressed in compress(input):
+            sio.write(compressed)
+        sio.seek(0)
+
+        result = decompress_data(sio)
+        self.assertEqual(result, '')
+
+if __name__ == '__main__':
+    unittest.main()
