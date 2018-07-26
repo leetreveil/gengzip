84a667dbff8bc115c3a84617b31c712a997aad8c
Fabien Chereau <fabien.chereau@gmail.com>
2018-07-26 18:03:48 +0200

    Made the library compatible with python3
    Tested with python 2.7.15rc1 and 3.6.5

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import unittest
-import StringIO
+import six
 import gzip
 from gengzip import compress
 
@@ -12,22 +12,22 @@ def decompress_data(fileobj):
 class tests(unittest.TestCase):
 
     def test_should_be_able_to_compress_a_list_of_data(self):
-        input = ['123', '45']
+        input = [b'123', b'45']
 
-        sio = StringIO.StringIO()
+        sio = six.BytesIO()
         for compressed in compress(input):
             sio.write(compressed)
         sio.seek(0)
 
         result = decompress_data(sio)
-        self.assertEqual(result, ''.join(input))
+        self.assertEqual(result, b''.join(input))
 
     def test_should_be_able_to_compress_data_coming_from_a_generator(self):
-        input = '12345'
+        input = b'12345'
         def gen():
             yield input
 
-        sio = StringIO.StringIO()
+        sio = six.BytesIO()
         for compressed in compress(gen()):
             sio.write(compressed)
         sio.seek(0)
@@ -36,18 +36,18 @@ class tests(unittest.TestCase):
         self.assertEqual(result, input)
 
     def test_should_be_able_to_compress_0_bytes_of_data(self):
-        input = ['']
+        input = [b'']
 
-        sio = StringIO.StringIO()
+        sio = six.BytesIO()
         for compressed in compress(input):
             sio.write(compressed)
         sio.seek(0)
 
         result = decompress_data(sio)
-        self.assertEqual(result, '')
+        self.assertEqual(result, b'')
 
     def test_should_be_able_to_set_the_compression_level(self):
-        list(compress('', compresslevel=1))
+        list(compress(b'', compresslevel=1))
 
 if __name__ == '__main__':
     unittest.main()
